34e12ffcc5d327b837bd124eeb3703d8e312d9c9
DecimalPoint <zubairuabduljelil2017@gmail.com>
2024-10-25 10:33:30 -0700

    Blog Post worked on

--- Blog/settings.py
+++ Blog/settings.py
@@ -117,11 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# media files (Images, Videos and Audio)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from . import models
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1 style='background: blue;'> This is it </h1>")
+    posts = models.Post.objects.all()
+    return render(request, 'app/index.html', {'posts': posts})
+
+
+def blog(request, id):
+    post = get_object_or_404(models.Post, id=id)
+    post.views += 1
+    post.save()
+    return render(request, 'app/blog.html', {'post': post})
